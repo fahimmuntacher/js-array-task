d4b91e8bd26b9523172d2a5811052b645b765fb3
fahimmuntacher <muntacherfahim234@gmail.com>
2025-07-31 15:08:07 +0600

    task 2 done

---
+++ includes.js
@@ -0,0 +1,7 @@
+let books = ["javascript book", "sci-fi book", "chokher bali", "feluda somogro", "pother pachali"];
+if(books.includes("javascript book")){
+    console.log("this array contains a javascript books")
+}
+else{
+    console.log("This array don't contain any javascript book")
+}
