807177aae8fde5d246013b3cac759d189b513079
fahimmuntacher <muntacherfahim234@gmail.com>
2025-07-31 15:24:29 +0600

    concat practise

---
+++ concat.js
@@ -0,0 +1,4 @@
+let generalSubjetcs = ["bangla", "english", "math", "Science"];
+let groupSubject = ["physics", "Chemistry", "Biology", "Higher math"]; 
+let allSubjects = generalSubjetcs.concat(groupSubject); 
+console.log("Here is the all combined subject:", allSubjects);
